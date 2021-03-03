e113fa14918fa0b67b3f4454b10e3d998f7a8d9e
MarinaGrifell <marinagrifell@hotmail.com>
2021-03-03 21:10:04 +0100

    create exercici5

---
+++ exercici5.js
@@ -0,0 +1,9 @@
+
+
+let notaexamen = prompt("Introdueix la nota d'exàmen");
+if (notaexamen >= 5) {
+     alert("Has aprovat l'exàmen amb un : "+notaexamen);
+ } else
+    alert(`Has suspés l'exàmen amb un : ${notaexamen}`);
+ 
+
